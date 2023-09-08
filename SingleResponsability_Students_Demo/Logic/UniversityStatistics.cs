0b47d10db597bfe42e9d34c60db4bf3d856444a5
Elena Olivia Balasoiu <ElenaOlivia.Balasoiu@traceone.com>
2023-09-08 17:25:29 +0300

    Add project files.

---
+++ SingleResponsability_Students_Demo/Logic/UniversityStatistics.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsability_Students_Demo.Logic
+{
+    public class UniversityStatistics
+    {
+        University university;
+
+        public UniversityStatistics(University university)
+        {
+            this.university = university;
+        }
+
+        #region Statistics
+
+        public double MinAdmissionMark
+        {
+            get { return university.Students.Min(student => student.AdmissionMark); }
+        }
+
+        public double MaxAdmissionMark
+        {
+            get { return university.Students.Max(student => student.AdmissionMark); }
+        }
+
+        #endregion
+    }
+}
---
+++ SingleResponsability_Students_Demo/Logic/UniversityToFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsability_Students_Demo.Logic
+{
+    public class UniversityToFile
+    {
+        public void SaveStudentsToFile(University university, string fullFilePath)
+        {
+            using (var writer = new StreamWriter(fullFilePath))
+            {
+                foreach (var s in university.Students)
+                {
+                    writer.WriteLine($"{s.Id},{s.FirstName},{s.LastName},{s.AdmissionMark.ToString(CultureInfo.InvariantCulture)}");
+                }
+            }
+        }
+    }
+}
